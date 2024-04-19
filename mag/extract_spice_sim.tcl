458fcfcb88b3b3fcd18918cd9700a7ca136c68b9
Nanik <nanik@pop-os.localdomain>
2024-04-18 22:20:51 -0400

    all the magic files... sorry for the mess I should probably organize this at some point

---
+++ mag/extract_spice_sim.tcl
@@ -0,0 +1,18 @@
+set project [lindex $argv $argc-2]
+set output [lindex $argv $argc-1]
+load $project.mag
+flatten tt_um_flat
+load tt_um_flat
+select top cell
+cellname delete $project
+cellname rename tt_um_flat ${project}
+extract all
+ext2sim labels on
+ext2sim
+extresist tolerance 10
+extresist
+ext2spice lvs
+ext2spice cthresh 0
+ext2spice extresist on
+ext2spice -o $output
+quit -noprompt
